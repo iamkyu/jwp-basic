65b76fa98cce1d9a2a32db4904da7435fb54db30
kjnam <kyujnam@gmail.com>
2016-11-10 20:46:58 +0900

    재학습을 위하여 소스코드를 upstream의 step0-getting-started 브랜치 상태로 되돌림

--- src/main/java/core/db/DataBase.java
+++ src/main/java/core/db/DataBase.java
@@ -1,22 +1,14 @@
 package core.db;
 
-import com.google.common.collect.Maps;
-import next.model.User;
-
 import java.util.Collection;
 import java.util.Map;
 
+import com.google.common.collect.Maps;
+
+import next.model.User;
+
 public class DataBase {
     private static Map<String, User> users = Maps.newHashMap();
-
-    static {
-        User admin = new User("admin", "admin", "admin", "admin@mail.com");
-        User user1 = new User("user1", "user1", "user1", "user1@mail.com");
-        User user2 = new User("user2", "user2", "user2", "user2@mail.com");
-        DataBase.addUser(admin);
-        DataBase.addUser(user1);
-        DataBase.addUser(user2);
-    }
 
     public static void addUser(User user) {
         users.put(user.getUserId(), user);
--- src/main/java/next/dao/UserDao.java
+++ src/main/java/next/dao/UserDao.java
@@ -1,37 +1,67 @@
 package next.dao;
 
-import next.dao.jdbc.JdbcTemplate;
-import next.dao.jdbc.RowMapper;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import core.jdbc.ConnectionManager;
 import next.model.User;
 
-import java.util.List;
-
 public class UserDao {
-    public void insert(User user) {
-        JdbcTemplate jdbcTemplate = new JdbcTemplate();
-        String sql = "INSERT INTO USERS VALUES (?, ?, ?, ?)";
-        jdbcTemplate.update(sql, user.getUserId(), user.getPassword(), user.getName(), user.getEmail());
+    public void insert(User user) throws SQLException {
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        try {
+            con = ConnectionManager.getConnection();
+            String sql = "INSERT INTO USERS VALUES (?, ?, ?, ?)";
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, user.getUserId());
+            pstmt.setString(2, user.getPassword());
+            pstmt.setString(3, user.getName());
+            pstmt.setString(4, user.getEmail());
+
+            pstmt.executeUpdate();
+        } finally {
+            if (pstmt != null) {
+                pstmt.close();
+            }
+
+            if (con != null) {
+                con.close();
+            }
+        }
     }
 
-    public void update(User user) {
-        JdbcTemplate jdbcTemplate = new JdbcTemplate();
-        String sql = "UPDATE USERS SET password=?, name =?, email=? WHERE userId=? ";
-        jdbcTemplate.update(sql, user.getPassword(), user.getName(), user.getEmail(), user.getUserId());
-    }
+    public User findByUserId(String userId) throws SQLException {
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            con = ConnectionManager.getConnection();
+            String sql = "SELECT userId, password, name, email FROM USERS WHERE userid=?";
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, userId);
 
-    public List<User> findAll() {
-        RowMapper<User> rm = rs -> new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
-                rs.getString("email"));
-        JdbcTemplate jdbcTemplate = new JdbcTemplate();
-        String sql =  "SELECT userId, password, name, email FROM USERS";
-        return jdbcTemplate.query(sql, rm);
-    }
+            rs = pstmt.executeQuery();
 
-    public User findByUserId(String userId) {
-        RowMapper<User> rm = rs -> new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
-                rs.getString("email"));
-        JdbcTemplate jdbcTemplate = new JdbcTemplate();
-        String sql =  "SELECT userId, password, name, email FROM USERS WHERE userid=?";
-        return jdbcTemplate.queryForObject(sql, rm, userId);
+            User user = null;
+            if (rs.next()) {
+                user = new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
+                        rs.getString("email"));
+            }
+
+            return user;
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (pstmt != null) {
+                pstmt.close();
+            }
+            if (con != null) {
+                con.close();
+            }
+        }
     }
 }
