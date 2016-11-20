31764162ed1144e8d2d7dea2aefaf80c9c17581c
kjnam <kyujnam@gmail.com>
2016-11-20 20:20:21 +0900

    feat(answers): 답변목록 읽어오도록 구현

--- src/main/java/next/controller/ShowController.java
+++ src/main/java/next/controller/ShowController.java
@@ -1,11 +1,14 @@
 package next.controller;
 
 import core.mvc.Controller;
+import next.dao.AnswerDao;
 import next.dao.QuestionDao;
+import next.model.Answer;
 import next.model.Question;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 /**
  * @author Kj Nam
@@ -14,10 +17,14 @@ import javax.servlet.http.HttpServletResponse;
 public class ShowController implements Controller {
     @Override
     public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
-        String questionId = req.getParameter("questionId");
+        long questionId = Long.parseLong(req.getParameter("questionId"));
         QuestionDao dao = new QuestionDao();
-        Question quesiton = dao.findByQuestionId(Long.valueOf(questionId));
+        Question quesiton = dao.findByQuestionId(questionId);
         req.setAttribute("question", quesiton);
+
+        AnswerDao answerDao = new AnswerDao();
+        List<Answer> answers = answerDao.findAllByQuestionId(questionId);
+        req.setAttribute("answers", answers);
 
         return "/qna/show.jsp";
     }
