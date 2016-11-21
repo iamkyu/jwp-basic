4ff9473c0695a182e69be385322b4349abcb4608
kjnam <kyujnam@gmail.com>
2016-11-21 12:34:00 +0900

    fix: 중복코드 제거

--- src/main/java/next/controller/qna/AddAnswerController.java
+++ src/main/java/next/controller/qna/AddAnswerController.java
@@ -1,6 +1,5 @@
 package next.controller.qna;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import core.mvc.ModelAndView;
 import next.controller.user.AbstractController;
 import next.dao.AnswerDao;
@@ -10,29 +9,23 @@ import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.PrintWriter;
 
 /**
  * @author Kj Nam
  * @since 2016-11-20
  */
 public class AddAnswerController extends AbstractController {
-    private static final Logger log =
-            LoggerFactory.getLogger(AddAnswerController.class);
+    private static final Logger log = LoggerFactory.getLogger(AddAnswerController.class);
+
+    private AnswerDao answerDao = new AnswerDao();
 
     @Override
-    public ModelAndView execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
-        Answer answer = new Answer(req.getParameter("writer"),
-                req.getParameter("contents"),
-                Long.valueOf(req.getParameter("questionId")));
-        log.debug("answer: {}", answer);
+    public ModelAndView execute(HttpServletRequest req, HttpServletResponse response) throws Exception {
+        Answer answer = new Answer(req.getParameter("writer"), req.getParameter("contents"),
+                Long.parseLong(req.getParameter("questionId")));
+        log.debug("answer : {}", answer);
 
-        AnswerDao answerDao = new AnswerDao();
         Answer savedAnswer = answerDao.insert(answer);
-        ObjectMapper mapper = new ObjectMapper();
-        resp.setContentType("application/json;charset=UTF-8");
-        PrintWriter out = resp.getWriter();
-        out.print(mapper.writeValueAsString(savedAnswer));
         return jsonView().addObject("answer", savedAnswer);
     }
 }
