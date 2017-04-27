a27e2e490b02b5cde888d7b58393bc01642ec0e4
iamkyu <kyujnam@gmail.com>
2017-04-27 20:43:00 +0900

    요구사항1. 리플렉션을 통한 메소드 및 필드 정보 획득

--- src/test/java/core/ref/ReflectionTest.java
+++ src/test/java/core/ref/ReflectionTest.java
@@ -7,6 +7,9 @@ import org.slf4j.LoggerFactory;
 import next.model.Question;
 import next.model.User;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
 
@@ -14,6 +17,16 @@ public class ReflectionTest {
     public void showClass() {
         Class<Question> clazz = Question.class;
         logger.debug(clazz.getName());
+
+        Method[] methods = clazz.getMethods();
+        for (Method method : methods) {
+            logger.debug(method.getName());
+        }
+
+        Field[] fields = clazz.getDeclaredFields();
+        for (Field field : fields) {
+            logger.debug(field.getName());
+        }
     }
     
     @Test
