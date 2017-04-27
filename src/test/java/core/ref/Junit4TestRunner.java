fe5bfe9c7268354dcd4b7f63372aae9f037f9609
iamkyu <kyujnam@gmail.com>
2017-04-27 20:50:18 +0900

    요구사항3. 특정 어노테이션의 메소드를 실행하는 러너구현

--- src/test/java/core/ref/Junit4TestRunner.java
+++ src/test/java/core/ref/Junit4TestRunner.java
@@ -2,10 +2,20 @@ package core.ref;
 
 import org.junit.Test;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+
 public class Junit4TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
+        Method[] methods = clazz.getMethods();
+        for (Method method : methods) {
+            Annotation annotation = method.getAnnotation(MyTest.class);
+            if (annotation != null) {
+                method.invoke(clazz.newInstance());
+            }
+        }
 
     }
 }
