f423a2b53481ec423310292dcc42967a971f195b
iamkyu <kyujnam@gmail.com>
2017-05-04 09:53:30 +0900

    어노테이션 기반의 컨트롤러 스캔

--- src/main/java/core/nmvc/AnnotationHandlerMapping.java
+++ src/main/java/core/nmvc/AnnotationHandlerMapping.java
@@ -1,16 +1,22 @@
 package core.nmvc;
 
-import java.util.Map;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+import core.annotation.RequestMapping;
+import core.annotation.RequestMethod;
+import org.reflections.ReflectionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
-
-import com.google.common.collect.Maps;
-
-import core.annotation.RequestMethod;
+import java.lang.reflect.Method;
+import java.util.Map;
+import java.util.Set;
 
 public class AnnotationHandlerMapping {
-    private Object[] basePackage;
+    private static final Logger logger = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
 
+    private Object[] basePackage;
     private Map<HandlerKey, HandlerExecution> handlerExecutions = Maps.newHashMap();
 
     public AnnotationHandlerMapping(Object... basePackage) {
@@ -18,7 +24,29 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
+        ControllerScanner scanner = new ControllerScanner(basePackage);
+        Map<Class<?>, Object> controllers = scanner.getControllers();
+        Set<Method> methods = getRequestMappingMethods(controllers.keySet());
+        for (Method method : methods) {
+            RequestMapping rm = method.getAnnotation(RequestMapping.class);
+            logger.debug("register handlerExecution : url is {}, method is {}", rm.value(), method);
+            handlerExecutions.put(createHandlerKey(rm),
+                    new HandlerExecution(controllers.get(method.getDeclaringClass()), method));
+        }
+        logger.info("Initialized AnnotationHandlerMapping!");
+    }
 
+    private HandlerKey createHandlerKey(RequestMapping rm) {
+        return new HandlerKey(rm.value(), rm.method());
+    }
+
+    private Set<Method> getRequestMappingMethods(Set<Class<?>> controllers) {
+        Set<Method> requestMappingMethods = Sets.newHashSet();
+        for (Class<?> clazz : controllers) {
+            requestMappingMethods
+                    .addAll(ReflectionUtils.getAllMethods(clazz, ReflectionUtils.withAnnotation(RequestMapping.class)));
+        }
+        return requestMappingMethods;
     }
 
     public HandlerExecution getHandler(HttpServletRequest request) {
---
+++ src/main/java/core/nmvc/ControllerScanner.java
@@ -0,0 +1,41 @@
+package core.nmvc;
+
+import com.google.common.collect.Maps;
+import core.annotation.Controller;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author Kj Nam
+ * @since 2017-05-05
+ */
+public class ControllerScanner {
+    private static final Logger logger = LoggerFactory.getLogger(ControllerScanner.class);
+    Reflections reflections;
+
+    public ControllerScanner(Object... basePackage) {
+        reflections = new Reflections(basePackage);
+    }
+
+    public Map<Class<?>, Object> getControllers() {
+        Set<Class<?>> preInitiatedControllers = reflections.getTypesAnnotatedWith(Controller.class);
+        return instantiateControllers(preInitiatedControllers);
+    }
+
+    Map<Class<?>, Object> instantiateControllers(Set<Class<?>> preInitiatedControllers) {
+        Map<Class<?>, Object> controllers = Maps.newHashMap();
+        try {
+            for (Class<?> clazz : preInitiatedControllers) {
+                controllers.put(clazz, clazz.newInstance());
+            }
+        } catch (InstantiationException | IllegalAccessException e) {
+            logger.error(e.getMessage());
+        }
+
+        return controllers;
+    }
+}
